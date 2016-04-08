710b07ba89101bd84e48e1b64a21c24273f107e9
Ahmad Hakroosh <ahmad@ahmad-h.com>
2016-04-08 12:48:16 +0300

    added a new class 'ClassName' for generating class names

--- SearsExam/Controllers/AppController.cs
+++ SearsExam/Controllers/AppController.cs
@@ -6,44 +6,25 @@ using System.Net;
 using System.Web.Mvc;
 using System.IO;
 using System.Text.RegularExpressions;
+using SearsExam.Models;
 
 namespace SearsExam.Controllers
 {
     public class AppController : Controller
     {
-        private string GetContent(string url)
-        {
-            string result;
-
-            HttpWebRequest webrequest = (HttpWebRequest)HttpWebRequest.Create(url);
-            webrequest.Method = "GET";
-            webrequest.ContentLength = 0;
-
-            WebResponse response = webrequest.GetResponse();
-
-            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
-            {
-                result = stream.ReadToEnd();
-            }
-
-            result = Regex.Replace(result, @"\r\n?|\n", "");
-
-            return result;
-        }
-
         // GET: App
         public ActionResult Index()
         {
-            ViewBag.result = GetContent("http://classnamer.com/index.txt");
+            ViewBag.result = new ClassName().className ;
             return View();
         }
 
         public ActionResult ThreeClass()
         {
-            List<string> results = new List<string>();
+            List<ClassName> results = new List<ClassName>();
 
             for (int i = 0; i < 3; i++)
-                results.Add(GetContent("http://classnamer.com/index.txt"));
+                results.Add(new ClassName());
 
             ViewBag.results = results;
 
@@ -54,16 +35,16 @@ namespace SearsExam.Controllers
         {
             //variables to be used for my calculation
             Dictionary<string, int> tags = new Dictionary<string, int>();
-            string className;
             List<string> classes = new List<string>();
+            ClassName className;
             string[] words;
             //get 200 classes
             for (int i = 0; i < 200; i++)
             {
-                className = GetContent("http://classnamer.com/index.txt");
-                classes.Add(className);
+                className = new ClassName();
+                classes.Add(className.className);
                 //split the class name by words
-                words = Regex.Split(className, @"(?<!^)(?=[A-Z])");
+                words = Regex.Split(className.className, @"(?<!^)(?=[A-Z])");
                 foreach (var word in words)
                 {
                     if (tags.ContainsKey(word))
---
+++ SearsExam/Models/ClassName.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace SearsExam.Models
+{
+    public class ClassName
+    {
+        public string className { get; private set; }
+
+        public ClassName()
+        {
+            className = getClassName();
+        }
+
+        private string getClassName()
+        {
+            string url = "http://classnamer.com/index.txt";
+            string result;
+
+            HttpWebRequest webrequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            webrequest.Method = "GET";
+            webrequest.ContentLength = 0;
+
+            WebResponse response = webrequest.GetResponse();
+
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                result = stream.ReadToEnd();
+            }
+
+            result = Regex.Replace(result, @"\r\n?|\n", "");
+
+            return result;
+        }
+    }
+}
